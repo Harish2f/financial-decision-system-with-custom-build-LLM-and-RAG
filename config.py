29e79385cfb8849aaaff27f3b016c550695710b7
Harish <harish@Harishs-MacBook-Air.local>
2026-01-13 19:09:57 +0100

    updated gitignore for CI

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_NAME = "finance_ai"
-DB_USER = "harish"
-DB_PASSWORD = ""
+import os
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", "5432"))
+DB_NAME = os.getenv("DB_NAME", "finance_ai")
